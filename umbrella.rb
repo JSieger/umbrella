8766b58f9d7a3b160018fa11c26334bb5796d4b6
Joseph Sieger <jsieger44@gmail.com>
2023-12-07 21:38:54 +0000

    got the pirate response

--- umbrella.rb
+++ umbrella.rb
@@ -11,13 +11,18 @@ location = "sacramento"
 google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + ENV.fetch("GMAPS_KEY")
 
 # Place a GET request to the URL
-raw_response = HTTP.get(google_maps_url)
+gmaps_response = HTTP.get(google_maps_url)
 
-gmaps_hash = JSON.parse(raw_response)
+gmaps_hash = JSON.parse(gmaps_response)
 
 gmaps_hash.fetch("results")[0]["geometry"]["location"]
 
 lat_long = gmaps_hash.fetch("results")[0]["geometry"]["location"]
 lat = lat_long["lat"]
 long = lat_long["lng"]
-pp "latitude: #{lat}, longitude: #{long}"
+
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + ENV.fetch("PIRATE_WEATHER_KEY") + "/#{lat},#{long}"
+
+pirate_response = HTTP.get(pirate_weather_url)
+
+pp pirate_response
